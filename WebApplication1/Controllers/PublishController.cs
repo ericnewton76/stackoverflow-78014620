df0fffcb38f7d0d5bcfb766014a668985d7fad05
Eric Newton <ericnewton76@gmail.com>
2024-02-18 00:27:36 -0500

    initial commit

---
+++ WebApplication1/Controllers/PublishController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+	public class PublishController : ApiController
+	{
+
+		[HttpPost]
+		[Route("api/profile/{profileId}/publish")]
+		public IHttpActionResult PostPublish([FromBody] PublishOptions options, int profileId, [FromUri] string publishers = null)
+		{
+			if (options == null) options = new PublishOptions(); //guard against null
+
+			if (options.PublishConfig != null)
+			{
+				var x = 1; //never gets here, PublishConfig always null
+			}
+
+
+			return Ok();
+		}
+
+	}
+
+	public class PublishOptions
+	{
+		public bool WaitForAll { get; set; }
+		public Newtonsoft.Json.Linq.JToken PublishConfig { get; set; }
+	}
+}
